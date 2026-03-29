9d635773aa05c165cc8315319428cc7faca2d529
Tim Jang <timjang3@gmail.com>
2026-03-29 10:48:26 -0700

    refactor: extract shared runtime core

--- packages/opensteer/src/captcha/types.ts
+++ packages/opensteer/src/captcha/types.ts
@@ -1,13 +1,1 @@
-import type { CaptchaProvider, CaptchaType } from "@opensteer/protocol";
-
-export interface CaptchaSolveRequest {
-  readonly type: CaptchaType;
-  readonly siteKey: string;
-  readonly pageUrl: string;
-  readonly signal?: AbortSignal;
-}
-
-export interface CaptchaSolverAdapter {
-  readonly provider: CaptchaProvider;
-  solve(input: CaptchaSolveRequest): Promise<{ readonly token: string }>;
-}
+export * from "../../../runtime-core/src/captcha/types.js";
